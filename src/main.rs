e950c338a429dde58939adbc9227dca9d47891a1
Dixaba <dixaba@yandex.ru>
2021-01-28 00:29:06 +0000

    Oof

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,152 @@
 #[macro_use]
 extern crate log;
 
-use daemonize::Daemonize;
-use std::convert::Infallible;
+use rusqlite::{params, Connection, Result, NO_PARAMS};
+use std::collections::HashMap;
+use std::fs::File;
+use std::io::prelude::*;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
-use warp::http::StatusCode;
-use warp::{reject, Filter, Rejection, Reply};
+use std::{fs, io};
+use warp::{http::Uri, Filter};
 
-#[cfg(not(debug_assertions))]
-static WEB_ROOT: &str = "/var/sudoku/";
-#[cfg(debug_assertions)]
+#[derive(Debug)]
+struct FileClasses {
+    id: i32,
+    name: String,
+    viewed: u32,
+    oof: u32,
+    ooyoy: u32,
+    aba: u32,
+}
+
 static WEB_ROOT: &str = "./assets/";
 
-static PAGE404: &str = r"
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>404 - not found</title>
-    </head>
-    <body>
-        <h1>File not found</h1>
-    </body>
-</html>
-";
+fn fill_data(data: Option<HashMap<String, String>>) -> String {
+    let conn = Connection::open("./db.sqlite").unwrap();
 
-async fn p404(_: Rejection) -> Result<impl Reply, Infallible> {
-    Ok(warp::reply::with_status(
-        warp::reply::html(PAGE404),
-        StatusCode::NOT_FOUND,
-    ))
+    if let Some(x) = data {
+        if !x.is_empty() {
+            info!("Got user response {:?}", x);
+
+            let response = FileClasses {
+                id: 0,
+                name: x.get("filename").expect("Empty shiet").to_string(),
+                oof: x.get("oof").is_some() as u32,
+                ooyoy: x.get("ooyoy").is_some() as u32,
+                aba: x.get("aba").is_some() as u32,
+
+                viewed: 1,
+            };
+
+            conn.execute(
+                "INSERT INTO files(name, oof, ooyoy, aba, viewed) VALUES(?1, ?2, ?3, ?4, 1)
+                    ON CONFLICT(name) DO UPDATE SET oof=oof+?2, ooyoy=ooyoy+?3, aba=aba+?4, viewed=viewed+1",
+                params![response.name, response.oof, response.ooyoy, response.aba],
+            )
+            .unwrap();
+        };
+    };
+
+    let mut sel = conn.prepare("SELECT * FROM files").unwrap();
+    let files_iter = sel
+        .query_map(params![], |row| {
+            Ok(FileClasses {
+                id: row.get(0).unwrap(),
+                name: row.get(1).unwrap(),
+                viewed: row.get(2).unwrap(),
+                oof: row.get(3).unwrap(),
+                ooyoy: row.get(4).unwrap(),
+                aba: row.get(5).unwrap(),
+            })
+        })
+        .unwrap();
+
+    let mut out = String::new();
+
+    for file in files_iter {
+        out = format!("{}{:?}\n", out, file.unwrap());
+    }
+
+    out
 }
 
 #[tokio::main]
 async fn main() {
     pretty_env_logger::env_logger::from_env(
-        pretty_env_logger::env_logger::Env::default().default_filter_or("warn"),
+        pretty_env_logger::env_logger::Env::default().default_filter_or("info"),
     )
     .init();
 
-    let daemonize = Daemonize::new()
-        .pid_file("/run/sudoku.pid")
-        .chown_pid_file(true)
-        .user("nobody")
-        .group("daemon");
+    let conn = Connection::open("./db.sqlite").unwrap();
+    conn.execute(
+        "CREATE TABLE files (
+            id      INTEGER PRIMARY KEY,
+            name    VARCHAR(255) UNIQUE,
+            viewed  INTEGER,
+            oof     INTEGER,
+            ooyoy   INTEGER,
+            aba     INTEGER
+        )",
+        params![],
+    )
+    .unwrap_or_default();
 
-    match daemonize.start() {
-        Ok(_) => println!("Success, daemonized"),
-        Err(e) => eprintln!("Error, {}", e),
+    let entries = fs::read_dir(WEB_ROOT.to_owned() + "audios")
+        .unwrap()
+        .map(|res| res.map(|e| e.path()))
+        .collect::<Result<Vec<_>, io::Error>>()
+        .unwrap();
+
+    for entry in entries {
+        let fname = entry.to_str().unwrap();
+        let fname = fname.replace(WEB_ROOT, "");
+        info!("Found file {:?}", fname);
+
+        conn.execute(
+            "INSERT OR IGNORE INTO files(name, oof, ooyoy, aba, viewed) VALUES(?1,0,0,0,0)",
+            &[fname],
+        )
+        .unwrap();
     }
 
-    let assets = warp::fs::dir(WEB_ROOT)
-        .map(|file: warp::filters::fs::File| {
-            info!("{:#?}", file);
-            file
-        })
-        .recover(p404);
+    conn.close().unwrap();
 
-    let routes = warp::get().and(assets);
+    let assets = warp::fs::dir(WEB_ROOT).map(|file: warp::filters::fs::File| {
+        info!("{:#?}", file);
+        file
+    });
+
+    let form =
+        warp::path("form")
+            .and(warp::body::form())
+            .map(|form_data: HashMap<String, String>| {
+                let mut file = File::open(WEB_ROOT.to_owned() + "index.html").unwrap();
+                let mut file_data = String::new();
+                file.read_to_string(&mut file_data).unwrap();
+
+                let data = file_data.replace("!!!", &fill_data(Some(form_data)));
+                let conn = Connection::open("./db.sqlite").unwrap();
+                let least_viewed: String = conn
+                    .query_row(
+                        "SELECT name FROM files ORDER BY viewed ASC",
+                        NO_PARAMS,
+                        |row| row.get(0),
+                    )
+                    .unwrap();
+
+                info!("Selected file {:?} as least viewed", least_viewed);
+
+                let data = data.replace("###", &least_viewed);
+                conn.close().unwrap();
+
+                warp::reply::html(data)
+            });
+
+    let index = warp::path::end().map(|| warp::redirect(Uri::from_static("/form")));
+    let gets = warp::get().and(index.or(form).or(assets));
+    let posts = warp::post().and(form);
+
+    let routes = gets.or(posts);
 
     info!("Serving static files from {}", WEB_ROOT);
 
